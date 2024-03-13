74b5d8e15233830c87d99ab0459eeeada76b8a48
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2024-03-13 12:06:17 +0700

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+const electron = require('electron')
+const { app, BrowserWindow, ipcMain, ipcRenderer, screen } = require("electron");
+const fs = require('fs');
+const path = require('path');
+var win
+
+function createWindow() {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    win = new BrowserWindow({
+        icon: './costume2.png',
+        autoHideMenuBar: true,
+        width: width,
+        height: height,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeIntegration: true,
+            devTools: true,
+            contextIsolation: false,
+            nodeIntegrationInSubFrames: true,
+            nodeIntegrationInWorker: true,
+            webviewTag: true
+        }
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    screen.on('display-metrics-changed', (event, display, changedMetrics) => {
+        if (changedMetrics.size && !currentScreenSize.equals(changedMetrics.size)) {
+            const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+            win.setSize(width, height)
+            console.log('resized')
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
